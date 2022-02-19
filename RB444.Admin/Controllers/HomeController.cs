dd05047efe1ee59926ad195a292e907e543c9575
Arun Verma <vermaarun1995@gmail.com>
2022-02-19 11:35:26 +0530

    Setup Admin Project

---
+++ RB444.Admin/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RB444.Admin.Models;
+using RB444.Data.Entities;
+using System.Diagnostics;
+
+namespace RB444.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly UserManager<Users> _userManager;
+
+        public HomeController(UserManager<Users> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RB444.Admin/Startup.cs
@@ -0,0 +1,119 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RB444.Core.IServices;
+using RB444.Core.Services;
+using RB444.Data;
+using RB444.Data.Entities;
+using RB444.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace RB444.Admin
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMemoryCache();
+            services.AddSession();
+            services.AddHttpContextAccessor();
+
+            services.AddDbContext<RB444Context>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            //    .AddEntityFrameworkStores<RB444Context>();
+
+            services.AddMvc();
+            ////Set Session Timeout. Default is 20 minutes.
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+            });
+
+            services.AddTransient<IUserStore<Users>, UserStore>();
+            services.AddTransient<IRoleStore<ApplicationRole>, RoleStore>();
+
+            services.AddIdentity<Users, ApplicationRole>(options =>
+            {
+                options.User.RequireUniqueEmail = false;
+            })
+            .AddDefaultTokenProviders();
+
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+
+            services.AddTransient<IRequestServices, RequestServices>();
+            services.AddTransient<ILoggerService, LoggerService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSession();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            var supportedCultures = new List<CultureInfo> { };
+            var gb = new CultureInfo("en-US");
+            gb.DateTimeFormat.ShortDatePattern = "dd/MM/yyyy HH:mm:ss";
+            gb.DateTimeFormat.DateSeparator = "/";
+            supportedCultures.Add(gb);
+
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                // Formatting numbers, dates, etc.
+                SupportedCultures = supportedCultures,
+                // UI strings that we have localized.
+                SupportedUICultures = supportedCultures,
+            });
+            app.Use(async (context, next) =>
+            {
+                context.Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture("en-US"))
+                );
+                await next();
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
