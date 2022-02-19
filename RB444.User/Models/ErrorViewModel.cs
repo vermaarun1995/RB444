babc4428c91dcf4f3f47bb192706da6f7b41b6f2
Arun Verma <vermaarun1995@gmail.com>
2022-02-19 11:46:57 +0530

    Project Setup User

---
+++ RB444.User/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RB444.User.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
