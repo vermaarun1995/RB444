13cc81c8f60c841fd4bcbf7a9b28680fe16895d2
Arun Verma <vermaarun1995@gmail.com>
2022-02-19 11:45:59 +0530

    Project Setup Model

---
+++ RB444.Model/Model/AccountModel.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace RB444.Models.Model
+{
+    public class UserModel
+    {
+        public int userid { get; set; }
+        public string fullname { get; set; }
+        public string email { get; set; }
+        public string phoneno { get; set; }
+        public string address { get; set; }
+        public int roleid { get; set; }
+        public string access_token { get; set; }
+    }
+
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email can't be blank")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string email { get; set; }
+
+        [Required(ErrorMessage = "Password can't be blank")]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+
+        public bool rememberme { get; set; }
+    }
+
+    public class VerifyEmailModel
+    {
+        public int UserId { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class ResetPasswordModel
+    {
+        public string NewPassword { get; set; }
+        public string OldPassword { get; set; }
+    }
+
+    public class ForgotPasswordModel
+    {
+        public string EmailAddress { get; set; }
+    }
+
+    public class LoginWith2faViewModel
+    {
+        [Required]
+        [StringLength(7, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Authenticator code")]
+        public string TwoFactorCode { get; set; }
+
+        [Display(Name = "Remember this machine")]
+        public bool RememberMachine { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+
+    public class LoginWithRecoveryCodeViewModel
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Recovery Code")]
+        public string RecoveryCode { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        public int id { get; set; }
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        //[Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }        
+        public int RoleId { get; set; }
+        //public UserDetails userDetails { get; set; }
+        //public UserSocialLinks userSocialLinks { get; set; }
+        //public List<UserEducation> userEducations { get; set; }
+        //public List<UserVisaInfo> userVisaInfos { get; set; }
+    }
+
+    public class RegisterVM
+    {
+        public int id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public int CountryId { get; set; }
+        public string CountryName { get; set; }      
+    }
+
+    public class RegisterPagingVM
+    {
+        public List<RegisterVM> RegisterVMs { get; set; }
+        public int PreviousPage { get; set; }
+        public int NextPage { get; set; }
+        public int TotalRecord { get; set; }
+        public string ShowPageInfo { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int Start { get; set; }
+        public int End { get; set; }
+    }
+
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        public string UserId { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Old password")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public string Code { get; set; }
+    }
+}
---
+++ RB444.Model/Model/CommonModel.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RB444.Models.Model
+{
+    public class CommonModel
+    {
+    }
+    public class CommonReturnResponse
+    {
+        public Boolean IsSuccess { get; set; }
+        public int Status { get; set; }
+        public string Message { get; set; }
+        public dynamic Data { get; set; }
+    }
+
+    public class PriorityStatus
+    {
+        public int PriorityId { get; set; }
+        public string Priority { get; set; }
+        public string PriorityColourCode { get; set; }
+    }
+
+    public class SportsData
+    {
+        public string key { get; set; }
+        public string group { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public bool active { get; set; }
+        public bool has_outrights { get; set; }
+    }
+
+    public class Sports
+    {
+        public string group { get; set; }
+    }
+
+    public class Series
+    {
+        public string key { get; set; }
+        public string group { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public bool active { get; set; }
+        public bool has_outrights { get; set; }
+    }
+
+    public class Outcome
+    {
+        public string name { get; set; }
+        public double price { get; set; }
+    }
+
+    public class Market
+    {
+        public string key { get; set; }
+        public List<Outcome> outcomes { get; set; }
+    }
+
+    public class Bookmaker
+    {
+        public string key { get; set; }
+        public string title { get; set; }
+        public DateTime last_update { get; set; }
+        public List<Market> markets { get; set; }
+    }
+
+    public class MatchList
+    {
+        public string id { get; set; }
+        public string sport_key { get; set; }
+        public string sport_title { get; set; }
+        public DateTime commence_time { get; set; }
+        public string home_team { get; set; }
+        public string away_team { get; set; }
+    }
+
+    public class MatchOdds
+    {
+        public string id { get; set; }
+        public string sport_key { get; set; }
+        public string sport_title { get; set; }
+        public DateTime commence_time { get; set; }
+        public string home_team { get; set; }
+        public string away_team { get; set; }
+        public List<Bookmaker> bookmakers { get; set; }
+    }
+}
