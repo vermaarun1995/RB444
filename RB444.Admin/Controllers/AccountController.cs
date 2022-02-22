207f7419be9d915fdc2f3d0063b0a6fab5d8db0a
Arun Sharma <arunsharm143@gmail.com>
2022-02-22 23:42:44 +0530

    login account

--- RB444.Admin/Controllers/AccountController.cs
+++ RB444.Admin/Controllers/AccountController.cs
@@ -1,12 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RB444.Model.ViewModel;
+using System;
+using System.Threading.Tasks;
 
 namespace RB444.Admin.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Login(string returnUrl = null)
+        public ActionResult Login(string returnUrl = null)
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            try
+            {
+               
+                return View(model);
+            }
+            catch (Exception ex)
+            {                
+                return View(model);
+            }
         }
     }
 }
