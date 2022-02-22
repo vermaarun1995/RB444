5bcf7e0dfd75ea7f3974824c515bb6699bcd6b04
Arun Verma <vermaarun1995@gmail.com>
2022-02-22 21:32:06 +0530

    Setting Add

---
+++ RB444.Admin/Controllers/SettingController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RB444.Admin.Controllers
+{
+    public class SettingController : Controller
+    {
+        public IActionResult SportsSetting()
+        {
+            return View();
+        }
+
+        public IActionResult SeriesSetting()
+        {
+            return View();
+        }
+
+        public IActionResult MatchSetting()
+        {
+            return View();
+        }
+    }
+}
