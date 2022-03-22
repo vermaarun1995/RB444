38c85502887be5d4d0c41088eec7ce89245bcb38
Arun Sharma <arunsharm143@gmail.com>
2022-03-22 20:15:56 +0530

    commit account

--- RB444.Admin/Controllers/AccountController.cs
+++ RB444.Admin/Controllers/AccountController.cs
@@ -86,6 +86,7 @@ namespace RB444.Admin.Controllers
                                     UserId = user.Id,
                                     Status = "login_success"
                                 };
+
                                 var _result = await _baseRepository.InsertAsync(activityLog);
                                 if (_result > 0) { _baseRepository.Commit(); } else { _baseRepository.Rollback(); }
                             }
